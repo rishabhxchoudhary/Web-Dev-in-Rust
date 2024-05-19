184c8b0b35eeb57be07930cf176b03ffe3c40f41
Rishabh Kumar Choudhary <97455703+rk26072003@users.noreply.github.com>
2024-05-19 09:16:46 +0530

    hello world setup

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+use routes::create_route;
+
+
+
+mod routes;
+
+pub async fn run() {
+    let app = create_route();
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use web::run;
+
+
+#[tokio::main]
+async fn main() {
+    run().await;
+}
